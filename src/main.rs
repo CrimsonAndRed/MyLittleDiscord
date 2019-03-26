7f925dce80d7c2dd68e2bf9976de6950a79658f0
CrimsonAndRed <homa-alternativa@mail.ru>
2019-03-26 14:51:32 +0300

    Basic websocket example from docs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,18 @@ extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 
+extern crate futures;
+extern crate tokio;
+extern crate websocket;
+
 use log::{debug, info};
+use futures::future::Future;
+use futures::sink::Sink;
+use futures::stream::Stream;
+use websocket::result::WebSocketError;
+use websocket::{ClientBuilder, OwnedMessage};
+
+
 
 mod data;
 mod web;
@@ -17,5 +28,30 @@ fn main() -> Result<(), Box<std::error::Error>> {
         .get("https://discordapp.com/api/v6/users/@me/guild")?;
 
     info!("{}", &resp.text()?);
+    //--
+
+	let mut runtime = tokio::runtime::current_thread::Builder::new()
+		.build()
+        .unwrap();
+    debug!("Connecting to {}", &p.wss_ref);
+
+	let runner = ClientBuilder::new(&p.wss_ref)
+		.unwrap()
+		.async_connect_secure(None)
+		.and_then(|(duplex, _)| {
+			let (sink, stream) = duplex.split();
+			stream
+				.filter_map(|message| {
+					info!("Received Message: {:?}", message);
+				    match message {
+						OwnedMessage::Close(e) => Some(OwnedMessage::Close(e)),
+						OwnedMessage::Ping(d) => Some(OwnedMessage::Pong(d)),
+						_ => None,
+					}
+				})
+				.forward(sink)
+		});
+    //--
+    runtime.block_on(runner).unwrap();
     Ok(())
 }
